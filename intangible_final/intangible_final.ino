6886f1ca2538dff41edb249a613e6ed51f1f9c86
Michelle Wu <michellewu@10-23-11-157.dynapool.wireless.nyu.edu>
2023-05-10 16:55:04 -0400

    added lighting effect for wifi connection status

--- intangible_final/intangible_final.ino
+++ intangible_final/intangible_final.ino
@@ -30,21 +30,8 @@ int layer[4] = { A3, A2, A1, A0 };
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial);
-  while ( status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);                   // print the network name (SSID);
-
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-  }
-
-  Serial.println("Adafruit VL6180x test!");
-  if (! vl.begin()) {
-    Serial.println("Failed to find sensor");
-    while (1);
-  }
-  Serial.println("Sensor found!");
+  while (!Serial)
+    ;
 
   for (int i = 0; i < 16; i++) {
     pinMode(column[i], OUTPUT);
@@ -52,6 +39,22 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(layer[i], OUTPUT);
   }
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);  // print the network name (SSID);
+    aroundEdgeDown();
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("Adafruit VL6180x test!");
+  if (!vl.begin()) {
+    Serial.println("Failed to find sensor");
+    while (1)
+      ;
+  }
+  Serial.println("Sensor found!");
 
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
@@ -61,6 +64,47 @@ void setup() {
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
+}
+
+void loop() {
+  if (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);  // print the network name (SSID);
+    aroundEdgeDown();
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+  }
+
+  int sensorValue = readSensor();
+
+  if (sensorValue > 100) {
+    if (!handDetected) {
+      randomflicker();
+    } else {
+      post(0);
+      handDetected = false;
+      randomflicker();
+    }
+  } else {
+    if (handDetected) {
+      if (!get()) randomflicker();
+    } else {
+      handDetected = true;
+      post(1);
+      if (get()) diagonalRectangle();
+    }
+  }
+
+  Serial.println("Wait 0.5 seconds\n");
+  delay(50);
 }
 
 int readSensor() {
@@ -76,7 +120,7 @@ int readSensor() {
 }
 
 void post(int value) {
-    // assemble the path for the POST message:
+  // assemble the path for the POST message:
   String postPath = "/dweet/for/" + postName;
   String contentType = "application/json";
 
@@ -130,58 +174,28 @@ bool get() {
   return result;
 }
 
-void loop() {
-  int sensorValue = readSensor();
-
-  if (sensorValue > 100) {
-    if (!handDetected) {
-      return;
-    } else {
-      post(0);
-      handDetected = false;
-      randomflicker();
-    }
-  } else {
-    if (handDetected) {
-      if (!get()) randomflicker();
-    } else {
-      handDetected = true;
-      post(1);
-      if (get()) diagonalRectangle();
-    }
-  }
-
-  Serial.println("Wait 1.2 seconds\n");
-  delay(1200);
-}
-
-void randomflicker()
-{
+void randomflicker() {
   turnEverythingOff();
-  int x = 10;
-  for(int i = 0; i !=750; i+=2)
-  {
-  int randomLayer = random(0,4);
-  int randomColumn = random(0,16);
-  
-  digitalWrite(layer[randomLayer], 1);
-  digitalWrite(column[randomColumn], 0);
-  delay(x);
-  digitalWrite(layer[randomLayer], 0);
-  digitalWrite(column[randomColumn], 1);
-  delay(x); 
+  int x = 50;
+  for (int i = 0; i != 50; i += 2) {
+    int randomLayer = random(0, 4);
+    int randomColumn = random(0, 16);
+
+    digitalWrite(layer[randomLayer], 1);
+    digitalWrite(column[randomColumn], 0);
+    delay(x);
+    digitalWrite(layer[randomLayer], 0);
+    digitalWrite(column[randomColumn], 1);
+    delay(x);
   }
 }
 
-void diagonalRectangle()
-{
+void diagonalRectangle() {
   int x = 350;
   turnEverythingOff();
-  for(int count = 0; count<5; count++)
-  {
+  for (int count = 0; count < 5; count++) {
     //top left
-    for(int i = 0; i<8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[3], 1);
@@ -189,8 +203,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //middle middle
-    for(int i = 4; i<12; i++)
-    {
+    for (int i = 4; i < 12; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[1], 1);
@@ -198,8 +211,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //bottom right
-    for(int i = 8; i<16; i++)
-    {
+    for (int i = 8; i < 16; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[0], 1);
@@ -207,8 +219,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //bottom middle
-    for(int i = 4; i<12; i++)
-    {
+    for (int i = 4; i < 12; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[0], 1);
@@ -216,8 +227,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //bottom left
-    for(int i = 0; i<8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[0], 1);
@@ -225,8 +235,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //middle middle
-    for(int i = 4; i<12; i++)
-    {
+    for (int i = 4; i < 12; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[1], 1);
@@ -234,8 +243,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //top right
-    for(int i = 8; i<16; i++)
-    {
+    for (int i = 8; i < 16; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[2], 1);
@@ -243,8 +251,7 @@ void diagonalRectangle()
     delay(x);
     turnEverythingOff();
     //top middle
-    for(int i = 4; i<12; i++)
-    {
+    for (int i = 4; i < 12; i++) {
       digitalWrite(column[i], 0);
     }
     digitalWrite(layer[2], 1);
@@ -253,8 +260,7 @@ void diagonalRectangle()
     turnEverythingOff();
   }
   //top left
-  for(int i = 0; i<8; i++)
-  {
+  for (int i = 0; i < 8; i++) {
     digitalWrite(column[i], 0);
   }
   digitalWrite(layer[3], 1);
@@ -263,14 +269,61 @@ void diagonalRectangle()
   turnEverythingOff();
 }
 
-void turnEverythingOff()
- {
-   for(int i = 0; i<16; i++)
-   {
-     digitalWrite(column[i], 1);
-   }
-   for(int i = 0; i<4; i++)
-   {
-     digitalWrite(layer[i], 0);
-   }
- }
+void turnEverythingOff() {
+  for (int i = 0; i < 16; i++) {
+    digitalWrite(column[i], 1);
+  }
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(layer[i], 0);
+  }
+}
+
+void aroundEdgeDown() {
+  for (int x = 50; x != 0; x -= 50) {
+    turnEverythingOff();
+    for (int i = 4; i != 0; i--) {
+      digitalWrite(layer[i - 1], 1);
+      digitalWrite(column[5], 0);
+      digitalWrite(column[6], 0);
+      digitalWrite(column[9], 0);
+      digitalWrite(column[10], 0);
+
+      digitalWrite(column[0], 0);
+      delay(x);
+      digitalWrite(column[0], 1);
+      digitalWrite(column[4], 0);
+      delay(x);
+      digitalWrite(column[4], 1);
+      digitalWrite(column[8], 0);
+      delay(x);
+      digitalWrite(column[8], 1);
+      digitalWrite(column[12], 0);
+      delay(x);
+      digitalWrite(column[12], 1);
+      digitalWrite(column[13], 0);
+      delay(x);
+      digitalWrite(column[13], 1);
+      digitalWrite(column[15], 0);
+      delay(x);
+      digitalWrite(column[15], 1);
+      digitalWrite(column[14], 0);
+      delay(x);
+      digitalWrite(column[14], 1);
+      digitalWrite(column[11], 0);
+      delay(x);
+      digitalWrite(column[11], 1);
+      digitalWrite(column[7], 0);
+      delay(x);
+      digitalWrite(column[7], 1);
+      digitalWrite(column[3], 0);
+      delay(x);
+      digitalWrite(column[3], 1);
+      digitalWrite(column[2], 0);
+      delay(x);
+      digitalWrite(column[2], 1);
+      digitalWrite(column[1], 0);
+      delay(x);
+      digitalWrite(column[1], 1);
+    }
+  }
+}
